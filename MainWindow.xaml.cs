83e27b53860f807e9bba3fe8f09a178999c1f995
ElliottMcArthur <47399898+ElliottMcArthur@users.noreply.github.com>
2019-03-26 11:48:13 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drawing_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++) 
+                { 
+                    Rectangle a = new Rectangle();
+                    a.Height = 100;
+                    a.Width = 100;
+                    if ((j + i) % 2 == 0)
+                    {
+                        a.Fill = Brushes.Aqua;
+                    }
+
+                    else
+                    {
+                        a.Fill = Brushes.DarkRed;
+                    }
+
+                    Canvas.Children.Add(a);
+                    Canvas.SetLeft(a, i * 100);
+                    Canvas.SetTop(a, j * 100);
+                }
+            }
+        }
+    }
+}
